a1f0cae5be0380cd0742dacc7d1af71fba1143c3
Yehor Lebid <yehor.lebid.education@gmail.com>
2025-07-15 16:46:02 +0300

    implement agentic

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+    const summarizer = createAgent({
+        name: 'summarizer',
+        system: 'You summarize input in 2 words',
+        model: openai({ model: 'gpt-4o-mini' })
+    });
+
+    const { output } = await summarizer.run(
+        `Summarize the following text: ${event.data.value}`
+    );
+
+    return { output };
   },
 );
