201f27c8b0abd4c08245faaee60b3c0273096f9c
yehor-lebid-education <yehor.lebid.education@gmail.com>
2025-07-15 16:46:42 +0300

    implement agentic (#4)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Page() {
+    const [value, setValue] = useState<string>('');
+
     const trpc = useTRPC();
     const invoke = useMutation(trpc.invoke.mutationOptions({
         onSuccess: () => {
@@ -15,9 +19,10 @@ export default function Page() {
 
     return (
         <div className="p-4 max-w-7xl mx-auto">
+            <Input value={value} onChange={(e) => setValue(e.target.value)} />
             <Button
                 disabled={invoke.isPending}
-                onClick={() => invoke.mutate({ text: "From JOB" })}
+                onClick={() => invoke.mutate({ value })}
             >Invoke Background Job</Button>
         </div>
     )
